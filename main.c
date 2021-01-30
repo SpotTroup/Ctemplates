c42685ff5a215708497855642f6732cb3b307fa9
SpotTroup <75669991+SpotTroup@users.noreply.github.com>
2021-01-30 11:24:20 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,41 @@
-#include <stdio.h>
+//=========================================================================================
+/*
+    _          __             _            _ 
+   / \  _   _ / _| __ _  __ _| |__   ___  / |
+  / _ \| | | | |_ / _` |/ _` | '_ \ / _ \ | |
+ / ___ \ |_| |  _| (_| | (_| | |_) |  __/ | |
+/_/   \_\__,_|_|  \__, |\__,_|_.__/ \___| |_|
+                  |___/      
+*/
+// Name: mainA.
+// Comments: Main function for the task A
+// Author: Tjark Ziehm
+// Version: 0.01
+// Date: January. 2020
+// APT Install: Figlet
+// needed Extensions: (Visual Studio Code), Todo Tree, (or TODO Highlight ), GitLens,
+//                    Bracket Pair Colorizer, Prettier Formatter
+//=========================================================================================
+// Structure
+//=========================================================================================
 
-int main()
+/*
+// TODO 
+
+// FIXME
+*/
+
+//=========================================================================================
+
+#include <stdio.h> // EOF in stdio.h als -1 definiert
+#include <stdbool.h>
+#include <stdlib.h>
+
+#include "bib.h"
+
+int main(void)
 {
-    printf("Hello Workd!\n");
+    /* code here */
 
     return 0;
 }
